ba4b469a7f38867c7536ca8b566d3092dc55a9e4
Alexis Cepeda <alexis9932@gmail.com>
2024-06-30 18:12:20 -0600

    Lists page

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -6,7 +6,7 @@ import { prisma } from "@/lib/prisma";
 import { ActionResult } from "@/types";
 import { User } from "@prisma/client";
 import { LoginSchema } from "@/lib/schemas/loginSchema";
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import { AuthError } from "next-auth";
 
 export async function signInUser(
@@ -82,4 +82,13 @@ export async function getUserByEmail(email: string) {
 }
 export async function getUserById(id: string) {
   return prisma.user.findUnique({ where: { id } });
+}
+
+export async function getAuthUserId() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error("Unathorized");
+
+  return userId;
 }
